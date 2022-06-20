b83af6c2b6d81324a623aa218c91e999b6fcb0e3
Ezequiel Birman <ebirman77@gmail.com>
2022-06-19 22:08:15 -0300

    Rename tests
    All those “my” were affecting the readability. Oh my.

--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -10,69 +10,69 @@ class ProjectTest < ActiveSupport::TestCase
     assert_equal my_true.not, my_false
   end
 
-  def test_my_false_or_my_false_is_my_false
+  def test_false_or_false_is_false
     my_false = MyFalse.instance
     assert_equal my_false.my_or(my_false), my_false
   end
 
-  def test_my_false_or_my_true_is_my_true
+  def test_false_or_true_is_true
     my_false = MyFalse.instance
     my_true = MyTrue.instance
     assert_equal my_false.my_or(my_true), my_true
   end
 
-  def test_my_true_or_my_true_is_my_true
+  def test_true_or_true_is_true
     my_true = MyTrue.instance
     assert_equal my_true.my_or(my_true), my_true
   end
 
-  def test_my_true_or_my_false_is_my_true
+  def test_true_or_false_is_true
     my_true = MyTrue.instance
     my_false = MyFalse.instance
     assert_equal my_true.my_or(my_false), my_true
   end
 
-  def test_my_false_and_my_false_is_my_false
+  def test_false_and_false_is_false
     my_false = MyFalse.instance
     assert_equal my_false.my_and(my_false), my_false
   end
 
-  def test_my_true_and_my_true_is_my_true
+  def test_true_and_true_is_true
     my_true = MyTrue.instance
     assert_equal my_true.my_and(my_true), my_true
   end
 
-  def test_my_false_and_my_true_is_my_false
+  def test_false_and_true_is_false
     my_true = MyTrue.instance
     my_false = MyFalse.instance
     assert_equal my_false.my_and(my_true), my_false
   end
 
-  def test_my_true_and_my_false_is_my_false
+  def test_true_and_false_is_false
     my_true = MyTrue.instance
     my_false = MyFalse.instance
     assert_equal my_true.my_and(my_false), my_false
   end
 
-  def test_my_false_if_false_evaluates_its_block
+  def test_false_if_false_evaluates_its_block
     my_false = MyFalse.instance
     expected = "I am expected"
     assert_equal my_false.if_false { expected }, expected
   end
 
-  def test_my_false_if_true_does_not_evaluate_its_block
+  def test_false_if_true_does_not_evaluate_its_block
     my_false = MyFalse.instance
-    expected = "I am expected"
-    assert_nil(my_false.if_true { expected })
+    unexpected = "I am not expected"
+    assert_nil(my_false.if_true { unexpected })
   end
 
-  def test_my_true_if_true_evaluates_its_block
+  def test_true_if_true_evaluates_its_block
     my_true = MyTrue.instance
     expected = "I am expected"
     assert_equal my_true.if_true { expected }, expected
   end
 
-  def test_my_true_if_false_does_not_evaluate_its_block
+  def test_true_if_false_does_not_evaluate_its_block
     my_true = MyTrue.instance
     unexpected = "I am not expected"
     assert_nil(my_true.if_false { unexpected })
